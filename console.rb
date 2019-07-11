7637d570f2fcf1bac29d30d39935db400e59181d
RichardT74 <richtrist@gmail.com>
2019-07-11 14:54:49 +0100

    before update users

---
+++ console.rb
@@ -0,0 +1,21 @@
+require_relative("./models/movie")
+require_relative("./models/star")
+require_relative("./models/casting")
+
+require( 'pry-byebug' )
+
+Movie.delete_all()
+Star.delete_all()
+
+
+movie1 = Movie.new({ 'title' => 'Midsommar', 'genre' => 'Horror' })
+movie1.save
+
+
+star1 = Star.new({ 'first_name' => 'Florence', 'last_name' => 'Pugh'})
+star1.save
+
+
+
+binding.pry
+nil
